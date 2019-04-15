62622db29f0c6332ec122ecebac8a367250397fe
abufaris <abufaris@MacBook-Air-alkhas-b-abufaris.local>
2019-04-15 15:22:27 +0300

    homework w2d1

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-// turn on stop function 
 function turnOnStopLight() {
-    // change background color of stop light to red
-    // changes background color of green and yellow to black
+  // change background color of stop light to red
+  // changes background color of green and yellow to black
+var on = $('.bulb').css('background-color')
+ $('#stopLight').css('background-color','red');
+ $('#slowLight').css('background-color','black');
+ $('#goLight').css('background-color','black');
+ //  turnOffLights();
 }
-
+$('#stopButton').on('click', turnOnStopLight);
 // turn on slow function
 function turnOnSlowLight() { 
-  // change background color of slow light to yellow
-  // changes background color of red and green to black
+// change background color of slow light to yellow
+// changes background color of red and green to black
+var on = $('.bulb').css('background-color')
+$('#stopLight').css('background-color','black');
+$('#slowLight').css('background-color','yellow');
+$('#goLight').css('background-color','black');  
+// turnOffLights();
 }
-
+$('#slowButton').on('click', turnOnSlowLight);
 // turn on go function
 function turnOnGoLight() {
-    // change background color of go light to green
-    // changes background color of red and yellow to black
+  // change background color of go light to green
+  // changes background color of red and yellow to black
+  var on = $('.bulb').css('background-color')
+ $('#stopLight').css('background-color','black');
+ $('#slowLight').css('background-color','black');
+ $('#goLight').css('background-color','green');
+ 
+ // turnOffLights();
 }
 
-// set stop light click event
+$('#goButton').on('click', turnOnGoLight);
 
-// set slow light click event
-
-// set go light click event
 
